d56d1b24887a9d9bf08f2294867fc1c49a35cca0
EnderDev <kieran@dothq.co>
2020-12-06 16:31:26 +0000

    ✨ Initial commit

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use serenity::{
+    async_trait,
+    client::{
+        Client,
+        Context,
+        EventHandler
+    },
+    model::{
+        channel::{
+            Message
+        }
+    },
+    framework::standard::{
+        StandardFramework,
+        CommandResult,
+        macros::{
+            command,
+            group
+        }
+    }
+};
+
+use std::env;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~"))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_TOKEN").expect("Token environment variable is not set");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
